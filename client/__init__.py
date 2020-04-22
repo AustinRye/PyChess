38a68162fe53a3230693e0a0e5455269e52572d5
AustinWRye <RyeAustinW@gmail.com>
2020-04-22 02:03:45 -0400

    Add the main menu

--- client/__init__.py
+++ client/__init__.py
@@ -1,6 +1,7 @@
 import pygame
 
 from client.game import Game
+from client.game_states.main_menu import MainMenu
 from client.game_states.singleplayer import Singleplayer
 
 # Create display surface
@@ -9,8 +10,8 @@ pygame.display.set_caption('Online Multiplayer Chess')
 surface = pygame.display.set_mode(surface_size)
 
 # Create game states
-states = {'Singleplayer': Singleplayer()}
-start_state_name = 'Singleplayer'
+states = {'MainMenu': MainMenu(), 'Singleplayer': Singleplayer()}
+start_state_name = 'MainMenu'
 
 # Create the game
 game = Game(surface, states, start_state_name)
