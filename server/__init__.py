e39e29b49288f697d2ee4ebb6daf21f7e22c7bc2
AustinWRye <RyeAustinW@gmail.com>
2020-05-05 21:19:43 -0400

    Add server socket listening and client connect handler

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,14 @@
+import socket
+
 from server.server import Server
 
 
-server = Server()
+# IP and PORT of the server
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5000
+# Header length in bytes
+HEADER_LENGTH = 2
+# String encoding format
+FORMAT = 'UTF-8'
+
+server = Server(IP, PORT, HEADER_LENGTH, FORMAT)
--- server/server.py
+++ server/server.py
@@ -1,7 +1,49 @@
+import socket
+import threading
+
+
 class Server:
 
-    def __init__(self):
-        pass
+    def __init__(self, IP, PORT, HEADER_LENGTH, FORMAT):
+        self.ADDR = (IP, PORT)
+        self.HEADER_LENGTH = HEADER_LENGTH
+        self.FORMAT = FORMAT
+
+        self.connected = False
+
+        # Initialize the server socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(self.ADDR)
+
+    def handle_client(self, conn, addr):
+        print(f'[NEW CONNECTION] Client connected from {addr}')
+
+        while True:
+            with conn:
+                msg_length = conn.recv(self.HEADER).decode(self.FORMAT)
+                if not msg_length:
+                    break
+                msg = conn.recv(msg_length).decode(self.FORMAT)
+                conn.send('Msg received'.encode(self.FORMAT))
+
+        conn.close()
+
+    def listen(self):
+        """
+        Listen for incoming connections.
+        """
+        self.socket.listen()
+        print(f'[LISTENING] Server is listening on {self.ADDR}')
+
+        while True:
+            conn, addr = self.socket.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
 
     def start(self):
-        print('[START] Server started')
+        """
+        Start the server.
+        """
+        print('[STARTING] Server is starting...')
+
+        self.listen()
